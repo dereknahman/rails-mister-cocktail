7b97b716ecd4fd5e40ce6c21f03e6616e882c464
Kirsty Simmonds <kirstysimmonds@icloud.com>
2019-02-21 17:29:00 +0100

    Mister Cocktail -- Day One

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating Cocktails...'
+cocktails_attributes = [
+  {
+    name: 'Mojito'
+  },
+  {
+    name: 'Mint Julep'
+  },
+  {
+    name: 'Tom Collins'
+  },
+  {
+    name: 'Whiskey Sour'
+  }
+]
+
+Cocktail.create!(cocktails_attributes)
+puts 'Finished!'
+
+Ingredient.create(name: 'Lime wedge')
+Ingredient.create(name: 'Lemon wedge')
+Ingredient.create(name: 'Mint leaves')
+Ingredient.create(name: 'Maple syrup')
+Ingredient.create(name: 'Bourbon')
+Ingredient.create(name: 'Crushed ice')
+Ingredient.create(name: 'Toasted sugar')
+Ingredient.create(name: 'Egg white')
+Ingredient.create(name: 'White sugar')
+Ingredient.create(name: 'White rum')
+Ingredient.create(name: 'Club soda')
+Ingredient.create(name: 'Gin')
