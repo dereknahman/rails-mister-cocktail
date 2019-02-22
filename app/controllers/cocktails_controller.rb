60a97536f65248601d7d1cf89dc93857f1cafddf
Kirsty Simmonds <kirstysimmonds@icloud.com>
2019-02-22 18:57:46 +0100

    Mister Cocktail which needs the new cocktail page to redirect to the index

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,15 +15,14 @@ class CocktailsController < ApplicationController
 
   def new
     @cocktail = Cocktail.new
+    @dose = Dose.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+
   end
 
   def destroy
